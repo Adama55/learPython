0410c5f96430e88842066b9dc682d333d0cdb6c9
Adama55 <mafahfof55@gmail.com>
2023-11-29 00:10:43 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+m = "Mississippi"
+
+compteurs = {char: 0 for char in set(ct)} 
+
+for lettre in m:
+    if lettre in compteurs:
+        compteurs[lettre] += 1
+
+for char in sorted(compteurs):  # Parcours des clés triées du dictionnaire
+    print(f"Le nombre de '{char}' dans la chaîne est : {compteurs[char]}")
