20cc17a80bb22d5baf640f3c430030bb61990e4c
Adama55 <mafahfof55@gmail.com>
2023-11-29 12:50:51 +0100

    mot plus utilisé non pronoms

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+'''
+FOFANA Adama
+STEPHANE Pieraly
+LAWANI Imran
+MYSSIE Mondestin
+NDANDJI Franck
+DE SOUZA Yvann
+
+'''
+m = "Mississippi"
+# 2 afficher la fréquence d'apparition de chaque lettre
+s = {e: m.count(e) for e in m}
+print(s)
+
+#1-a Trouver la lettre la plus fréquente et sa fréquence
+
+max_freq = max(s.values())
+most_frequent_letters = [letter for letter, freq in s.items() if freq == max_freq]
+print(most_frequent_letters, "et leur fréquence d'apparition est", max_freq)
+
+#1-b Remplacer les lettres avec la fréquence maximale par "e"
+m_modifiee = m
+for lettre in most_frequent_letters:
+    m_modifiee = m_modifiee.replace(lettre, 'e')
+
+print(m_modifiee)
+
 texte =""" 
 Je vois là-bas un être sans tête qui grimpe à une perche sans fin.
 
@@ -17,7 +44,7 @@ lui doive être plus haïssable encore.
 Henri Michaux
 """
 
-#calcul du nombre du pronom "le"
+# 3-a calcul du nombre du pronom "le"
 def clean_text(text):
     text = text.replace("\n", " ")
     return text.split(" ")
@@ -37,14 +64,15 @@ for i in range(1, len(my_text_cleaned)):
 # Affichez le résultat
 print("Le nombre 'le' est :", compteur)
 
-#compter le nombre de e
+#3-b compter le nombre de e
 nbr_e = texte.count("e")
 print("le nombre de 'e' est :", nbr_e)
 
-#retirer tous les "e"
+# 3-c retirer tous les "e"
 texte_sans_e = texte.replace("e", " ")
 #print(texte_sans_e)
 
+##4 ecrire le rapport dans le fichier json
 import json
 
 data ={ "nbr_pronom_le": compteur, "nbr_e":nbr_e}
@@ -52,19 +80,17 @@ path = "rapport.json" #D:/ESTIAM/python_expert/tp_cours/rapport.json
 with open(path,'w') as file:
     json.dump(data, file)
 
+## 5 trouver le mot le plus utilisé
 import re
 from collections import Counter
-texte_sans_ponctuation = re.sub(r'[^\w\s]', '', texte)
-text_list = texte_sans_ponctuation.split()
-#print(text_list)
 mots = re.findall(r'\b\w+\b', texte.lower())
 mot_plus_utilise = Counter(mots).most_common(1)
 print("le mot le plus utilisé est :", mot_plus_utilise)
 
-########################"
+######### trouver le mot le plus utilisé sans pronoms
 print("#########")
 pronoms = ['je', 'tu', 'il', 'elle', 'on', 'nous', 'vous', 'ils', 'elles', 'me', 'te', 'se', 'le', 'la', 'les', 'lui', 'leur', 'eux', 'y', 'en', 'moi', 'toi', 'soi', 'toi-même']
 mots = re.findall(r'\b\w+\b', texte.lower())
 mots_non_pronoms = [mot for mot in mots if mot not in pronoms]
 mot_plus_utilise_non_pronom = Counter(mots_non_pronoms).most_common(1)
-print("le mot le plus utilisé non pronoms est :", mot_plus_utilise_non_pronom)
+print("le mot le plus utilisé sans pronoms est :", mot_plus_utilise_non_pronom)
